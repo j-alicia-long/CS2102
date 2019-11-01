f9ad450b06587c18701c8bcd2c00090e88050850
jo-lyn <e0007247@u.nus.edu>
2019-11-02 01:32:06 +0800

    fix path not defined

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const path = require('path');
 const { pool } = require('./config');
 
 const app = express();
