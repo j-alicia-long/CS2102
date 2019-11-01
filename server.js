899d9f4ed865e3bde5a728e29ed872d6035ba37e
jo-lyn <e0007247@u.nus.edu>
2019-11-02 00:11:52 +0800

    try to host server on heroku

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require('express');
+const path = require('path');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const { pool } = require('./config');
 
 const app = express();
+
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, 'src/build')));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -41,6 +45,6 @@ app
   .post(addUser);
 
 // Start server
-app.listen(process.env.PORT || 3002, () => {
+app.listen(process.env.PORT || 5000, () => {
   console.log(`Server listening`);
 });
