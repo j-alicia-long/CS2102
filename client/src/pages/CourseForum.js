a46dc23e1afd3fd56ab499f4afb173e10b3a21a3
jo-lyn <e0007247@u.nus.edu>
2019-11-10 23:46:10 +0800

    fix forum

--- client/src/pages/CourseForum.js
+++ client/src/pages/CourseForum.js
@@ -69,79 +69,6 @@ class CourseForum extends React.Component {
     return body;
   };
 
-  /*
-  render() {
-    return (
-      <div>
-        <div className = "body">
-          <CourseNavBar />
-          <div className="course-description">
-            <div>
-              {this.state.course_details.map(course => (
-                <div>
-                  <h2 style={{ textAlign: "left" }} key={course.cid}> {course.cid} FORUM</h2>
-                </div>
-              ))}
-              <div className="join-button">
-                <Nav.Link href="/ForumTopic" >
-                  <Button className="btn-lg">Create New Topic</Button>
-                </Nav.Link>
-              </div>
-              <div className="forum-layout">
-                <h4 style={{ textAlign: "left", color: "grey" }}> General Discussion</h4>
-              <Card className="forum-card">
-                  <div className="forum-description">
-                          <p className="forum-details">
-                            <h5 className="forum-title"> General Discussion</h5>
-                            <br></br>
-                            Visible until 10 Dec 2019
-                          </p>
-                          <p className="forum-threads" >
-                            <a href="/DiscussionThreads">
-                              <h6>2 Discussion Threads</h6>
-                            </a>
-                          </p>
-                          <p className="entry-date" >
-                            <h6>Last Post: 10 Nov 2019</h6>
-                            Re: Clarification of Finals.
-                          </p>
-                        </div>
-                </Card>
-                <Card className="forum-card">
-                  {this.state.forum_list.map((forum, i) => (
-                    this.state.threads_list.map(threads => (
-                      this.state.entries_list.map(entries => (
-
-                        <div className="forum-description">
-                          <p className="forum-details">
-                            <h5 className="forum-title" key={(forum.f_title), (forum.f_dscp), (forum.f_date)}>{forum.f_title}</h5>
-                            {forum.f_dscp}<br></br>
-                            Visible until {forum.f_date}
-                          </p>
-                          <p className="forum-threads" key={entries.num_e}>
-                            <a href="/DiscussionThreads">
-                              <h6>{entries.num_e} Discussion Threads</h6>
-                            </a>
-                          </p>
-                          <p className="entry-date" key={(threads.t_title), (entries.e_date)}>
-                            <h6>Last Post: {entries.e_date}</h6>
-                            Re: {threads.t_title}
-                          </p>
-                        </div>
-                      ))
-                    ))
-                  ))}
-                </Card>
-              </div>
-            </div>
-          </div>
-          </div>
-        </div>
-    );
-  }
-}
-*/
-
   render() {
     return (
       <div>
@@ -163,27 +90,37 @@ class CourseForum extends React.Component {
             </div>
           </div>
           <div className="forum-layout">
-            <h4 style={{ textAlign: "left", color: "grey" }}>
-              General Discussion
-            </h4>
-            <Card className="forum-card">
-              <div className="forum-description">
-                <p className="forum-details">
-                  <h5 className="forum-title">General Discussion</h5>
-                  Posts on module relevant topics.<br></br>
-                  Visible until 10 Dec 2019 11:50pm.
-                </p>
-                <p className="forum-threads">
-                  <Nav.Link href="/DiscussionThreads">
-                    <h6>2 Discussion Threads</h6>
-                  </Nav.Link>
-                </p>
-                <p className="entry-date">
-                  <h6>Last Post: 5 Nov 2019</h6>
-                  Re: Clarification on finals.
-                </p>
+            {this.state.forum_list.map(forum => (
+              <div>
+                <h4
+                  style={{ textAlign: "left", color: "grey" }}
+                  key={forum.f_title}
+                >
+                  {forum.f_title}
+                </h4>
+                <Card className="forum-card">
+                  <div className="forum-description">
+                    <p className="forum-details">
+                      <h5 className="forum-title" key={forum.f_title}>
+                        {forum.f_title}
+                      </h5>
+                      {forum.f_dscp}
+                      <br></br>
+                      Visible until {forum.f_date}.
+                    </p>
+                    <p className="forum-threads">
+                      <Nav.Link href="/DiscussionThreads">
+                        <h6>2 Discussion Threads</h6>
+                      </Nav.Link>
+                    </p>
+                    <p className="entry-date">
+                      <h6>Last Post: 6 Nov 2019</h6>
+                      Re: Clarification on Assignment 4
+                    </p>
+                  </div>
+                </Card>
               </div>
-            </Card>
+            ))}
             <h4 style={{ textAlign: "left", color: "grey" }}>
               Lecture Questions
             </h4>
@@ -244,37 +181,6 @@ class CourseForum extends React.Component {
                 </p>
               </div>
             </Card>
-            {this.state.forum_list.map(forum => (
-              <div>
-                <h4
-                  style={{ textAlign: "left", color: "grey" }}
-                  key={forum.f_title}
-                >
-                  {forum.f_title}
-                </h4>
-                <Card className="forum-card">
-                  <div className="forum-description">
-                    <p className="forum-details">
-                      <h5 className="forum-title" key={forum.f_title}>
-                        {forum.f_title}
-                      </h5>
-                      {forum.f_dscp}
-                      <br></br>
-                      Visible until {forum.f_date}.
-                    </p>
-                    <p className="forum-threads">
-                      <Nav.Link href="/DiscussionThreads">
-                        <h6>2 Discussion Threads</h6>
-                      </Nav.Link>
-                    </p>
-                    <p className="entry-date">
-                      <h6>Last Post: 6 Nov 2019</h6>
-                      Re: Clarification on Assignment 4
-                    </p>
-                  </div>
-                </Card>
-              </div>
-            ))}
           </div>
         </div>
       </div>
