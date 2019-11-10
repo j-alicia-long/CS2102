dae5440bd78317458a9930313ae43f2b9c8a95e0
jo-lyn <e0007247@u.nus.edu>
2019-11-10 23:00:02 +0800

    fix threads

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,17 @@
-const auth = require('./auth');
-const users = require('./users');
-const courses = require('./courses');
-const students = require('./students');
-const facilitators = require('./facilitators');
-const forum = require('./forum');
-const threads = require('./threads');
-const entries = require('./entries');
+const auth = require("./auth");
+const users = require("./users");
+const courses = require("./courses");
+const students = require("./students");
+const facilitators = require("./facilitators");
+const forum = require("./forum");
+const entries = require("./entries");
 
-module.exports = { auth, users, courses, students, facilitators, forum, threads, entries };
+module.exports = {
+  auth,
+  users,
+  courses,
+  students,
+  facilitators,
+  forum,
+  entries
+};
