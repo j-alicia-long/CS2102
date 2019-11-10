0b36b4cc287e2cf059a4c843561a3ab43b768524
wongyuqi <wongyuqi@u.nus.edu>
2019-11-10 15:58:47 +0800

    courseforum

--- client/src/pages/CourseForum.js
+++ client/src/pages/CourseForum.js
@@ -2,7 +2,7 @@ import React from "react";
 import "../App.css";
 import CourseNavBar from "./CourseNavBar";
 
-import { Button, Card, Nav } from "react-bootstrap";
+import { Button, Card, Nav, Navbar } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.css";
 
 class CourseForum extends React.Component {
@@ -93,6 +93,7 @@ class CourseForum extends React.Component {
     return body;
   };
 
+  /*
   render() {
     return (
       <div>
@@ -157,6 +158,137 @@ class CourseForum extends React.Component {
                 </Card>
               </div>
             </div>
+          </div>
+          </div>
+        </div>
+    );
+  }
+}
+*/
+
+  render() {
+    return (
+      <div>
+        <Navbar className="navbar">This is Nav Bar</Navbar>
+
+        <div className="body">
+        <CourseNavBar />
+          <div className="course-description" >
+            <div>
+              {this.state.course_details.map( course => 
+              <h2 style={{ textAlign: "left" }} key = {course.cid}> {course.cid} FORUM</h2>
+              )}
+            </div>
+            <div className="new-button">
+              <Nav.Link href="/ForumTopic" >
+              <Button className="btn-lg">Create New Topic</Button>
+              </Nav.Link>
+            </div>
+          </div>
+          <div className = "forum-layout">
+            <h4 style={{ textAlign: "left" , color: "grey"}}>General Discussion</h4>
+            <Card className="forum-card">
+                <div className = "forum-description">
+                    <p className = "forum-details">
+                        <h5 className = "forum-title">General Discussion</h5>
+                        Posts on module relevant topics.<br></br>
+                        Visible until 10 Dec 2019 11:50pm.
+                    </p>
+                    <p className = "forum-threads">
+                      <Nav.Link href="/DiscussionThreads">
+                        <h6>2 Discussion Threads</h6>
+                      </Nav.Link>
+                    </p>
+                    <p className = "entry-date">
+                        <h6>Last Post: 5 Nov 2019</h6>
+                        Re: Clarification on finals.
+                    </p>
+                </div>
+            </Card>
+            <h4 style={{ textAlign: "left" , color: "grey"}}>Lecture Questions</h4>
+            <Card className="forum-card">
+                <div className ="forum-description">
+                    <p className = "forum-details">
+                        <h5 className = "forum-title">Lectures</h5>
+                        Posts on Lectures.<br></br>
+                        Visible until 10 Dec 2019 11:50pm.
+                    </p>
+                    <p className = "forum-threads">
+                      <Nav.Link href="/DiscussionThreads">
+                        <h6>1 Discussion Threads</h6>
+                      </Nav.Link>
+                    </p>
+                    <p className = "entry-date">
+                        <h6>Last Post: 6 Nov 2019</h6>
+                        Re: Clarification on Lecture 9
+                    </p>
+                </div>
+            </Card>
+            <h4 style={{ textAlign: "left" , color: "grey"}}>Tutorial Questions</h4>
+            <Card className="forum-card">
+                <div className ="forum-description">
+                    <p className = "forum-details">
+                        <h5 className = "forum-title">Tutorials</h5>
+                        Posts on Tutorial Questions.<br></br>
+                        Visible until 10 Dec 2019 11:50pm.
+                    </p>
+                    <p className = "forum-threads">
+                      <Nav.Link href="/DiscussionThreads">
+                        <h6>0 Discussion Threads</h6>
+                      </Nav.Link>
+                    </p>
+                    <p className = "entry-date">
+                        
+                    </p>
+                </div>
+            </Card>
+            <h4 style={{ textAlign: "left" , color: "grey"}}>Assignment Questions</h4>
+            <Card className="forum-card">
+                <div className ="forum-description">
+                    <p className = "forum-details">
+                        <h5 className = "forum-title">Assignment</h5>
+                        Posts on Assignment Questions.<br></br>
+                        Visible until 10 Dec 2019 11:50pm.
+                    </p>
+                    <p className = "forum-threads">
+                      <Nav.Link href="/DiscussionThreads">
+                        <h6>2 Discussion Threads</h6>
+                      </Nav.Link>
+                    </p>
+                    <p className = "entry-date">
+                        <h6>Last Post: 6 Nov 2019</h6>
+                        Re: Clarification on Assignment 4
+                    </p>
+                </div>
+            </Card>
+            { this.state.forum_list.map( forum => 
+            <h4 style={{ textAlign: "left" , color: "grey"}} key = {forum.f_title}>{forum.f_title}</h4>
+            )}
+            <Card className="forum-card">
+                  {this.state.forum_list.map((forum, i) => (
+                    this.state.threads_list.map(threads => (
+                      this.state.entries_list.map(entries => (
+
+                        <div className="forum-description">
+                          <p className="forum-details">
+                            <h5 className="forum-title" key={(forum.f_title), (forum.f_dscp), (forum.f_date)}>{forum.f_title}</h5>
+                            {forum.f_dscp}<br></br>
+                            Visible until {forum.f_date}
+                          </p>
+                          <p className="forum-threads" key={entries.num_e}>
+                            <a href="/DiscussionThreads">
+                              <h6>{entries.num_e} Discussion Threads</h6>
+                            </a>
+                          </p>
+                          <p className="entry-date" key={(threads.t_title), (entries.e_date)}>
+                            <h6>Last Post: {entries.e_date}</h6>
+                            Re: {threads.t_title}
+                          </p>
+                        </div>
+                      ))
+                    ))
+                  ))}
+                </Card>
           </div>
           </div>
         </div>
